db6fbb14ba269c22de83773707297d8663c245d4
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-11-27 11:36:41 +0700

    Add Authentication with Keycloak

---
+++ be/src/main/java/com/tmn/taskmanagenow/config/KeycloakClientConfig.java
@@ -0,0 +1,45 @@
+package com.tmn.taskmanagenow.config;
+
+import com.tmn.taskmanagenow.model.KeycloakConfigProperties;
+import static org.keycloak.OAuth2Constants.PASSWORD;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(KeycloakConfigProperties.class)
+public class KeycloakClientConfig {
+
+    private final KeycloakConfigProperties keycloakPropsConfig;
+
+    public KeycloakClientConfig(KeycloakConfigProperties keycloakPropsConfig) {
+        this.keycloakPropsConfig = keycloakPropsConfig;
+    }
+
+    @Bean()
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8080")
+                .realm("TaskManageNow")
+                .clientId("taskmanagenow")
+                .grantType(PASSWORD)
+                .username("admin")
+                .password("admin")
+                .clientSecret("hZnEarrFgriBAeCLneyZQX4mjyVj5eY4")
+                .build();
+    }
+//    @Bean
+//    public Keycloak keycloak() {
+//        return KeycloakBuilder.builder()
+//                .serverUrl(keycloakPropsConfig.getAuthServerUrl())
+//                .realm(keycloakPropsConfig.getRealm())
+//                .clientId(keycloakPropsConfig.getResource())
+//                .grantType(PASSWORD)
+//                .username("admin")
+//                .password("123456")
+//                .clientSecret(keycloakPropsConfig.getCredentials().getSecret())
+//                .build();
+//    }
+}
