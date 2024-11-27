3f6a829dd6cf17bf07cb4fb5299eb0fef0a1f045
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2024-11-27 11:33:18 +0700

    Init Backend

---
+++ be/src/main/java/com/tmn/taskmanagenow/TaskmanagenowApplication.java
@@ -0,0 +1,12 @@
+package com.tmn.taskmanagenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagenowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagenowApplication.class, args);
+    }
+}
---
+++ be/src/test/java/com/tmn/taskmanagenow/TaskmanagenowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmn.taskmanagenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagenowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
